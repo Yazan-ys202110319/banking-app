d11386723adaa3c16f1f798b138864c9c54c225a
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-06-15 00:44:35 +0300

    Refactor MobileNav and Sidebar components to enhance navigation functionality and styling

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { cn } from '../../lib/utils';
+import { SheetClose } from './sheet';
 
 const Sidebar = ({ user }: SiderbarProps) => {
 
@@ -20,12 +21,12 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
             {sidebarLinks.map((item) => {
                 const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
-                return <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient' : isActive})}>
-                    <div className='relative size-6'>
-                        <Image src={item.imgURL} alt = {item.label} fill className = {cn({
+                return <Link href={item.route} key={item.label} className={cn('mobilenav-sheet_close w-full', {'bg-bank-gradient' : isActive})}>
+                    
+                        <Image width={20} height={20} src={item.imgURL} alt = {item.label} fill className = {cn({
                             'brightness-[3] invert-0': isActive})} />
-                    </div>
-                        <p className={cn('sidebar-label', {'!text-white' : isActive})}>
+                    
+                        <p className={cn('text-16 font-semibold text-black-2', {'text-white' : isActive})}>
                             {item.label}
                         </p>
                 </Link>
