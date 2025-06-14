eec455dda0e88394da507fae0778961b386cc154
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-06-14 17:22:52 +0300

    Add sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,19 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {
+    firstName: 'Yazan',
+    lastName: 'Alsaleh'
+  };
+
   return (
-   <main>
-     SIDEBAR   
+   <main className="flex h-screen w-full font-inter">
+     <Sidebar user = {loggedIn} />   
      {children}
    </main>
   );
